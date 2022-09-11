fde100f9109c1c18dc3ab8206e964f6d22bef727
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2022-09-11 19:58:00 +0900

    akinomisyutoku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const { setTimeout } = require('timers/promises');
 const tabletojson = require('tabletojson').Tabletojson;
-
+const cheerio = require('cheerio');
 
 (async () => {
 	  const browser = await puppeteer.launch({
@@ -51,19 +51,56 @@ const tabletojson = require('tabletojson').Tabletojson;
 		  await setTimeout(1000)
 	  ]);
 
-	  console.dir(await(await ((await page.$x(`//a[text() = "ふなばし三番瀬海浜公園"]`))[0]).getProperty('innerHTML')).jsonValue());
 	  await (await page.$x(`//a[text() = "ふなばし三番瀬海浜公園"]`))[0].click();
 	  await Promise.all([
 		  await setTimeout(1000)
 	  ]);
-
+	  
 	  const tabledata = await page.evaluate(() => document.querySelector('table[class="m_akitablelist"]').outerHTML)
-	  console.dir(tabledata);
 	  const tabledata_json = tabletojson.convert(tabledata,{stripHtmlFromCells:false})
-	  console.dir(tabledata_json)
-	  await Promise.all([
-		  await setTimeout(1000)
-	  ]);
+	  const yearmonth = cheerio.load(tabledata_json[0][0]["日曜日"]).text()
+	  console.log(yearmonth)
+	  tabledata_json[0].shift();
+	  console.log("nengetu syutoku -------------")
+	  const data = tabledata_json[0].filter(item=>!!item).filter((item)=>{
+            delete item["月曜日"]
+            delete item["火曜日"]
+            delete item["水曜日"]
+            delete item["木曜日"]
+            delete item["金曜日"]
+	    return item
+	  })
+	  console.log("doniti igai sakujo -------------")
+	  data.filter(item=>{
+            if(item["土曜日"] == "&nbsp;"){
+              delete item["土曜日"]
+	    }
+            if(item["日曜日"] == "&nbsp;"){
+              delete item["日曜日"]
+	    }
+	  })
+	  console.log(data)
+	  const arrayvar = [];
+	  console.log("hiduke nasi sakujo -------------")
+	  data.forEach(item=>{
+            //一部空き
+            //全て空き
+            //予約あり
+            //if(item["日曜日"]?.toString().match(/.*予約あり.*/)){
+            //  arrayvar.push(item["日曜日"])
+	    //}
+            //if(item["土曜日"]?.toString().match(/.*予約あり.*/)){
+            //  arrayvar.push(item["土曜日"])
+            //}
+            if(item["日曜日"]?.toString().match(/.*一部空き.*/)){
+              arrayvar.push(item["日曜日"])
+	    }
+            if(item["土曜日"]?.toString().match(/.*一部空き.*/)){
+              arrayvar.push(item["土曜日"])
+            }
+	  })
+	  console.dir(arrayvar)
+	  console.log("akinomi syutoku -------------")
 	  
 	  await browser.close();
 })();
